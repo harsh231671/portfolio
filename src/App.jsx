99be79ef4db111fd4e238282e9e901cc210bfb0e
harsh231671 <115443950+harsh231671@users.noreply.github.com>
2025-09-06 22:06:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div className='h-screen w-full flex justify-center items-center'>
+        <h1 className='text-5xl text-center'>Coming Soon!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
